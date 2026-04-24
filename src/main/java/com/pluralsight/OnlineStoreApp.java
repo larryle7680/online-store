428a7700358f506b6eb7af6b47530f0a15a5f5eb
Larry <larryle7680@gmail.com>
2026-04-24 08:19:07 -0500

    Created a switch statement for choice one.

--- src/main/java/com/pluralsight/OnlineStoreApp.java
+++ src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -8,15 +8,27 @@ static Scanner theScanner = new Scanner(System.in);
     }//END OF MAIN METHOD
 
     public static void storeMenu(){
-        //Display the store Menu
+        //Initiate storeMenu variables
+        boolean isRunning = false;
+
+        //Display the store Menu and store users choice
         System.out.println();
         System.out.println("=== Store Menu ===");
         System.out.println();
-        System.out.println("1. Display Menu");
+        System.out.println("1. Display Product");
         System.out.println("2. Display Cart");
         System.out.println("3. Close the Application");
         System.out.println();
         System.out.print("Please choose an option: ");
+        int usersChoice = theScanner.nextInt();
+
+        while(isRunning){
+            switch(usersChoice){
+                case 1:
+                    displayProduct();
+                    break;
+            }
+        }
 
 
     }//END OF storeMenu METHOD
