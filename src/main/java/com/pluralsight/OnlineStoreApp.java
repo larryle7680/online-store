408761f8d2e91b3b0b65ef4da466782d9d3ee0f2
Larry <larryle7680@gmail.com>
2026-04-24 08:15:18 -0500

    Imported a Scanner to store users input. SOUT users menu and store it into a variable.

--- src/main/java/com/pluralsight/OnlineStoreApp.java
+++ src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -1,12 +1,24 @@
 package com.pluralsight;
-
+import java.util.*;
 public class OnlineStoreApp {
-
+static Scanner theScanner = new Scanner(System.in);
     public static void main(String[] args) {
 
 
-    }//END OF MAIN CURLY BRACE
+    }//END OF MAIN METHOD
 
-    publ
+    public static void storeMenu(){
+        //Display the store Menu
+        System.out.println();
+        System.out.println("=== Store Menu ===");
+        System.out.println();
+        System.out.println("1. Display Menu");
+        System.out.println("2. Display Cart");
+        System.out.println("3. Close the Application");
+        System.out.println();
+        System.out.print("Please choose an option: ");
 
-}//END OF THE OnlineStoreApp CURLY BRACE
+
+    }//END OF storeMenu METHOD
+
+}//END OF THE OnlineStoreApp CLASS
