5d44d736082e69977cbb255260da55e64c7d13fb
Larry <larryle7680@gmail.com>
2026-04-24 12:11:38 -0500

    Made a subDisplayMenu. Made a switch/case staement for display all products in the inventory.

--- src/main/java/com/pluralsight/OnlineStoreApp.java
+++ src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -2,15 +2,21 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.SQLOutput;
 import java.util.*;
 public class OnlineStoreApp {
 static Scanner theScanner = new Scanner(System.in);
+    //Made an empty array list to hold our products
+    static ArrayList<Product> inventory = new ArrayList<Product>();
     public static void main(String[] args) {
 
-
+    //Call the Store menu
+        storeMenu();
     }//END OF MAIN METHOD
 
     public static void storeMenu(){
+        ArrayList<Product> inventory = getInventory();
+
         //Initiate storeMenu variables
         boolean isRunning = false;
 
@@ -35,12 +41,43 @@ static Scanner theScanner = new Scanner(System.in);
 
     }//END OF storeMenu METHOD
 
-    public void displayProduct(){
+    public static void displayProduct(){
+    //Import the loaded array into this method
+        ArrayList<Product> inventory= getInventory();
+
+        //Prompt the users what options they want
+        System.out.println();
+        System.out.println("=== Display Menu ===");
+        System.out.println();
+        System.out.println("1.List all items");
+        System.out.println("2.Display Cart");
+        System.out.println("Main Menu");
+        System.out.println();
+        System.out.print("Please choose an option: ");
+        int usersChoice = theScanner.nextInt();
+
+        //displaySubMenu while loop/switch statement
+        boolean isRunning = false;
+        while(!isRunning){
+            switch(usersChoice){
+                case 1:
+                    for(int i = 0; i < inventory.size(); i++){
+                        Product p = inventory.get(i);
+                        System.out.printf("Name: %s | Price: $%.2f | Department: %s\n",
+                                p.getProductName(),p.getProductPrice(),p.getDepartment());
+                    }
+                    break;
+                case 2:
+                    System.out.println("CART");
+                    break;
+                case 3:
+                    return;
+            }
+        }
 
     }//END OF displayProduct METHOD
-    public ArrayList<Product> getInventory(){
-        //Made an empty array list to hold our products
-        ArrayList<Product> inventory = new ArrayList<Product>();
+    public static ArrayList<Product> getInventory(){
+
 
         //Need to something to read the csv
         FileReader fileReader = null;
@@ -61,14 +98,21 @@ static Scanner theScanner = new Scanner(System.in);
 
                 //Add them into an Array
                 inventory.add(new Product(nameSplit,priceSplit,departmentSplit));
+
             }
 
+            //Close the bufReader
+            bufReader.close();
         } catch (Exception e) {
             System.out.println("Could not find that file");
             throw new RuntimeException(e);
         }
 
 
-    }
+        //return the inventory
+        return inventory;
+
+
+    }//END OF ArrayList<Product> METHOD
 
 }//END OF THE OnlineStoreApp CLASS
