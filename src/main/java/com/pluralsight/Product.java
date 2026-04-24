4af124ef029982bd04d1e996b43f9e69bf681364
Larry <larryle7680@gmail.com>
2026-04-24 08:03:41 -0500

    Added Properties to my Products Added a Constructor Added Setters/Getters

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,5 +1,41 @@
 package com.pluralsight;
 
 public class Product {
+    //Properties for my Products
+    String productName;
+    double productPrice;
+    String Department;
 
+    //Added a constructor
+    public Product(String productName, double productPrice, String department) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        Department = department;
+    }
+
+    //Added Setters/Getters
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
+    }
 }
